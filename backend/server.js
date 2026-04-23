b688eaeb02068f43e6a7d55e13b1ba92364a1b6e
RedaYasserSebaa <rdawe767@gmail.com>
2026-04-24 01:52:07 +0300

    feat: fix the workflow in github actions to build the frontend before electron app

--- backend/server.js
+++ backend/server.js
@@ -284,20 +284,21 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
-    "default-src 'self' http://localhost:7676; " +
+    "default-src 'self' http://localhost:7676 http://127.0.0.1:7676; " +
     "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.plyr.io; " +
     "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://cdn.plyr.io https://fonts.googleapis.com; " +
     "font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; " +
     "img-src 'self' data: blob: https://image.tmdb.org https://api.themoviedb.org https://*.tile.openstreetmap.org; " +
-    "media-src 'self' blob: http://localhost:7676; " +
-    "connect-src 'self' http://localhost:7676 https://api.themoviedb.org https://*.themoviedb.org;"
+    "media-src 'self' blob: http://localhost:7676 http://127.0.0.1:7676; " +
+    "connect-src 'self' http://localhost:7676 http://127.0.0.1:7676 ws://localhost:7676 https://api.themoviedb.org https://*.themoviedb.org;"
   );
   next();
 });
 
 // Fix for Chrome DevTools discovery warning/error
 app.get('/.well-known/appspecific/com.chrome.devtools.json', (req, res) => {
-  res.json({});
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({}));
 });
 
 // Auth Middleware
